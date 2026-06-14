c90df2013cfb287f774ab7159dc30fb2d117a49d
andostop <tou-and.13@hotmail.com>
2026-06-14 14:13:41 -0500

    KAN-14 OAuth Google funcional con MySQL

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,42 +3,29 @@
 namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
-use App\Models\Usuario;
 
 class AuthController extends Controller
 {
     public function redirect()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')
+            ->scopes([
+                'openid',
+                'profile',
+                'email'
+            ])
+            ->redirect();
     }
 
     public function callback()
     {
         $googleUser = Socialite::driver('google')->user();
 
-        $usuario = Usuario::where(
-            'email',
-            $googleUser->getEmail()
-        )->first();
-
-        if (!$usuario) {
-
-            $usuario = Usuario::create([
-                'nombre' => $googleUser->getName(),
-                'email' => $googleUser->getEmail(),
-                'google_id' => $googleUser->getId(),
-                'rol' => 1,
-                'estado' => 'activo'
-            ]);
-        }
-
         return response()->json([
-            'id' => $usuario->id,
-            'nombre' => $usuario->nombre,
-            'email' => $usuario->email,
-            'rol' => $usuario->rol,
-            'estado' => $usuario->estado,
-            'accessToken' => $googleUser->token
+            'googleId' => $googleUser->getId(),
+            'nombre' => $googleUser->getName(),
+            'email' => $googleUser->getEmail(),
+            'foto' => $googleUser->getAvatar()
         ]);
     }
 }
